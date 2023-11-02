3da520004bd86c3ac5ab0798bc42d29cd9223e08
phamtungthuy <94853261+phamtungthuy@users.noreply.github.com>
2023-11-02 15:42:19 +0700

    Cuisine and Accommodation UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("com.google.firebase:firebase-auth:22.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:x.y.z")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
