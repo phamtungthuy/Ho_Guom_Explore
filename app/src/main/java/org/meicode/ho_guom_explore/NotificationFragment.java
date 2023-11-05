f54cfb0401ae36bc7bea47b48444ca9878256f25
MinhKhoiUET <minhkhoi281103@gmail.com>
2023-11-05 15:08:07 +0700

    change UI of homepage and add some fragments

--- app/src/main/java/org/meicode/ho_guom_explore/NotificationFragment.java
+++ app/src/main/java/org/meicode/ho_guom_explore/NotificationFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class NotificationFragment extends Fragment {
 
@@ -14,6 +15,7 @@ public class NotificationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_notification, container, false);
     }
 }
