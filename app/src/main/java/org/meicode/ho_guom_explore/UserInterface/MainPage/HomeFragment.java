49bb8b9102821e51dc51099ccedde1fd7ec21e1d
MinhKhoiUET <minhkhoi281103@gmail.com>
2023-11-12 10:55:36 +0700

    navigate from fragment to activity and from activity to fragment; fix a little UI :)

--- app/src/main/java/org/meicode/ho_guom_explore/UserInterface/MainPage/HomeFragment.java
+++ app/src/main/java/org/meicode/ho_guom_explore/UserInterface/MainPage/HomeFragment.java
@@ -1,5 +1,7 @@
 package org.meicode.ho_guom_explore.UserInterface.MainPage;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +9,34 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import org.meicode.ho_guom_explore.R;
+import org.meicode.ho_guom_explore.UserInterface.SubPages.CommentActivity;
 
 public class HomeFragment extends Fragment {
+    Activity context;
+    ImageButton commentButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        context = getActivity();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+
+    public void onStart() {
+        super.onStart();
+        commentButton = (ImageButton) context.findViewById(R.id.btnComment);
+        commentButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, CommentActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/org/meicode/ho_guom_explore/UserInterface/SubPages/CommentActivity.java
+++ app/src/main/java/org/meicode/ho_guom_explore/UserInterface/SubPages/CommentActivity.java
@@ -1,16 +1,32 @@
 package org.meicode.ho_guom_explore.UserInterface.SubPages;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import org.meicode.ho_guom_explore.R;
 
 public class CommentActivity extends AppCompatActivity {
+    ImageButton backToHomePage;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
+
+        backToHomePage = findViewById(R.id.commentToHomePage);
+        backToHomePage.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new Fragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.firstContainer, fragment).commit();
+            }
+        });
     }
 }
