e040f155b3d02af38dd5541d0210d124e4816aae
MinhKhoiUET <minhkhoi281103@gmail.com>
2023-11-12 17:03:38 +0700

    add new animation for image in homepage and change the layout of categories

--- app/src/main/java/org/meicode/ho_guom_explore/UserInterface/SubPages/CommentActivity.java
+++ app/src/main/java/org/meicode/ho_guom_explore/UserInterface/SubPages/CommentActivity.java
@@ -1,5 +1,6 @@
 package org.meicode.ho_guom_explore.UserInterface.SubPages;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -10,6 +11,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import org.meicode.ho_guom_explore.R;
+import org.meicode.ho_guom_explore.UserInterface.MainPage.HomePageActivity;
+import org.meicode.ho_guom_explore.UserInterface.VisitPage.VisitActivity;
 
 public class CommentActivity extends AppCompatActivity {
     ImageButton backToHomePage;
@@ -19,13 +22,11 @@ public class CommentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_comment);
 
         backToHomePage = findViewById(R.id.commentToHomePage);
-        backToHomePage.setOnClickListener(new View.OnClickListener(){
-
+        backToHomePage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Fragment fragment = new Fragment();
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.firstContainer, fragment).commit();
+            public void onClick(View v) {
+                Intent intent = new Intent(CommentActivity.this, HomePageActivity.class);
+                startActivity(intent);
             }
         });
     }
