6a9933510964016958e98ab94c87164be4b7ff0b
MinhKhoiUET <minhkhoi281103@gmail.com>
2023-11-12 15:04:11 +0700

    add logic for navigating from categories to the activity

---
+++ app/src/main/java/org/meicode/ho_guom_explore/UserInterface/EventPage/EventActivity.java
@@ -0,0 +1,15 @@
+package org.meicode.ho_guom_explore.UserInterface.EventPage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.meicode.ho_guom_explore.R;
+
+public class EventActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+    }
+}
--- app/src/main/java/org/meicode/ho_guom_explore/UserInterface/MainPage/HomeFragment.java
+++ app/src/main/java/org/meicode/ho_guom_explore/UserInterface/MainPage/HomeFragment.java
@@ -13,11 +13,21 @@ import android.widget.Button;
 import android.widget.ImageButton;
 
 import org.meicode.ho_guom_explore.R;
+import org.meicode.ho_guom_explore.UserInterface.CuisineAndAccommodationPage.CuisineAndAccommodation;
+import org.meicode.ho_guom_explore.UserInterface.EventPage.EventActivity;
+import org.meicode.ho_guom_explore.UserInterface.IntroductionPage.DetailActivity;
+import org.meicode.ho_guom_explore.UserInterface.ServicePage.ServiceActivity;
 import org.meicode.ho_guom_explore.UserInterface.SubPages.CommentActivity;
+import org.meicode.ho_guom_explore.UserInterface.VisitPage.VisitActivity;
 
 public class HomeFragment extends Fragment {
     Activity context;
     ImageButton commentButton;
+    ImageButton eventButton;
+    ImageButton serviceButton;
+    ImageButton cuisineAndAccommodationButton;
+    ImageButton introductionButton;
+    ImageButton visitButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -30,11 +40,54 @@ public class HomeFragment extends Fragment {
     public void onStart() {
         super.onStart();
         commentButton = (ImageButton) context.findViewById(R.id.btnComment);
-        commentButton.setOnClickListener(new View.OnClickListener() {
-
+        eventButton = (ImageButton) context.findViewById(R.id.btnEvent);
+        serviceButton = (ImageButton) context.findViewById(R.id.btnService);
+        introductionButton = (ImageButton) context.findViewById(R.id.btnIntroduction);
+        visitButton = (ImageButton) context.findViewById(R.id.btnVisit);
+        cuisineAndAccommodationButton = (ImageButton) context.findViewById(R.id.btnCuisineAndAccommodation);        commentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(context, CommentActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        eventButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, EventActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        serviceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, ServiceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cuisineAndAccommodationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, CuisineAndAccommodation.class);
+                startActivity(intent);
+            }
+        });
+
+        introductionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        visitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, VisitActivity.class);
                 startActivity(intent);
             }
         });
