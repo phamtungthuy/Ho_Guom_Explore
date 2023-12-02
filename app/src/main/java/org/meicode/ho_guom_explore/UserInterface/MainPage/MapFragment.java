99a7ea1922577a0c5e358d5b5f723418d0be7c0b
MinhKhoiUET <minhkhoi281103@gmail.com>
2023-12-02 17:35:04 +0700

    using gg map in map fragment and the phone number can be clickable

--- app/src/main/java/org/meicode/ho_guom_explore/UserInterface/MainPage/MapFragment.java
+++ app/src/main/java/org/meicode/ho_guom_explore/UserInterface/MainPage/MapFragment.java
@@ -1,27 +1,68 @@
 package org.meicode.ho_guom_explore.UserInterface.MainPage;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.GeolocationPermissions;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import org.meicode.ho_guom_explore.R;
 
 public class MapFragment extends Fragment {
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1001;
+    Activity context;
     WebView webView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        context = getActivity();
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        return view;
+    }
 
-//        webView.setWebViewClient(new WebViewClient());
-//        String mapUrl = "https://www.google.com/maps/search/?api=1&query=Hồ+Gươm";
-//        webView.loadUrl(mapUrl);
-        return inflater.inflate(R.layout.fragment_map, container, false);
+    public void onStart() {
+        super.onStart();
+
+        WebView webView = context.findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setGeolocationEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+
+            @SuppressLint("NewApi")
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                request.grant(request.getResources());
+            }
+        });
+
+        String mapUrl = "https://www.google.com/maps/search/?api=1&query=Hồ+Gươm";
+        webView.loadUrl(mapUrl);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                String mapUrl = "https://www.google.com/maps/search/?api=1&query=Hồ+Gươm";
+                WebView webView = context.findViewById(R.id.webView);
+                webView.loadUrl(mapUrl);
+            }
+        }
     }
 }
